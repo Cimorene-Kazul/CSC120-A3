b3ec0544a19959c9ee69b594565401425ab85bc6
Cimorene-Kazul <eliana.lippa@gmail.com>
2025-09-18 11:36:02 -0400

    Adds testing file

---
+++ Test.java
@@ -0,0 +1,30 @@
+// this is a file to let me test random numbers and other things that I might need.
+// I am leaving this here so that you can see my thought processes.
+public class Test {
+    String testing = "10";
+    Test(){
+    }
+    public static void main(String[] args) {
+        System.out.println((int)(Math.random()*3+1)); // Testing the random nunber built in that I had to look up
+
+        // testing saving transcripts
+        // A couple of forgetting ; bugs appeared
+        String test_string = "";
+        test_string += "Hi! \n";
+        System.out.println(test_string);
+        test_string += "Again.";
+        System.out.println(test_string);
+
+        // checking to see if values can be preset
+        Test myTest = new Test();
+        System.out.println(myTest.testing);
+        // Answer affirmative! no need to define variables all Tests have in the declaration. :)
+
+        //how do I work with lists test
+        String[] testlist = {"1", "2", "3"}; // Discovered lists use curly braces?
+        System.out.println(testlist[1]); // but you index the Python way
+        // figuring out lists of lists
+        String[][] testlist2 = {{"1", "2"}};
+        System.out.println(testlist2[0][1]);
+    }
+}
